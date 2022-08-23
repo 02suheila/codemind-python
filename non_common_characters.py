7fe3abc1731b22f3c2ddb1e793c9921647dcad6c
Suheila Mukthyarunnisa .SHAIK <84657568+02suheila@users.noreply.github.com>
2022-08-23 13:30:58 +0530

    s1=input().lower() s2=input().lower() a=set(s1) b=set(s2) g=''
    for i in a:
        if i ==' ':
            continue
        if i not in b:
            g+=i       
    for i in b:
        if i==' ':
            continue
        if i not in a:
            g+=i
    b=''.join(sorted(g))
    print(b)

---
+++ non_common_characters.py
@@ -0,0 +1,22 @@
+s1=input().lower()
+s2=input().lower()
+a=set(s1)
+b=set(s2)
+g=''
+
+for i in a:
+    if i ==' ':
+        continue
+    if i not in b:
+        g+=i       
+for i in b:
+    if i==' ':
+        continue
+    if i not in a:
+        g+=i
+b=''.join(sorted(g))
+print(b)
+
+
+
+    
